da4ff7d58ae10926c86736a13fc5f5f31c4929f8
Naoki Uchida <106541486+naomam@users.noreply.github.com>
2022-06-03 18:07:02 +0900

    新規クラス作成

---
+++ src/Chapter05.java
@@ -0,0 +1,13 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		String name = "内田 直希";
+		System.out.println(name);		
+	
+		name = "おにぎり";
+		System.out.println(name);
+	
+		final int luckyNumber = 12;
+		System.out.println(luckyNumber);
+	}
+}
