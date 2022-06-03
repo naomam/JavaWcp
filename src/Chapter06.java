fbfbe0ae2c2c921561d2bacc765dbf6821625052
Naoki Uchida <106541486+naomam@users.noreply.github.com>
2022-06-03 23:59:18 +0900

    新規クラス作成

---
+++ src/Chapter06.java
@@ -0,0 +1,28 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	long long1 = 10000000000L;
+	System.out.println(long1);
+	
+	double double1 = 3.14;
+	System.out.println(double1);
+	
+	boolean boolean1 = true;
+	System.out.println(boolean1);
+	
+	Integer integer1 = 100;
+	System.out.println(integer1);
+	
+	Float float1 = 1.4142f;
+	System.out.println(float1);
+	
+	Character character1 = 'う';
+	System.out.println(character1);
+	
+	int[] array1 = {0, 10, 20, 30, 40 };
+	System.out.println(array1[3]);
+	}
+
+}
