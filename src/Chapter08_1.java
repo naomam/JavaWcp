5bfa513019dfa7d3191456c1d4fc79fcb557bdc8
Naoki Uchida <106541486+naomam@users.noreply.github.com>
2022-06-05 21:57:53 +0900

    新規クラス作成

---
+++ src/Chapter08_1.java
@@ -0,0 +1,18 @@
+
+public class Chapter08_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number = 12;
+		if (number <= 5) {
+			System.out.println("とても近いです");
+		} else if (number <= 10) {
+			System.out.println("近いです");
+		} else if(number <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+
+}
